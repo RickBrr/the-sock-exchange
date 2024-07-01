0d171f24c5966a42303dff0fb347fe05a06effd5
Rick Bernard <rick.bernard527@gmail.com>
2024-07-01 11:07:39 -0400

    MERN #10 - Navigator second try

--- react/src/components/Navigator.jsx
+++ react/src/components/Navigator.jsx
@@ -1,4 +1,12 @@
-<div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
-    <button className='btn btn-primary'>Back</button>
-    <button className='btn btn-primary'>Next</button>
-</div>
+import React from "react";
+
+const Navigator = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+            <button className='btn btn-primary'>Back</button>
+            <button className='btn btn-primary'>Next</button>
+        </div>
+    )
+}
+
+export default Navigator;
